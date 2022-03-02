0f4dd4f0bb0bf122602023a889345f99043b9d34
rkskek1356 <96719726+rkskek1356@users.noreply.github.com>
2022-03-03 00:10:21 +0900

    Delete bootstrapTemplate directory

--- bootstrapTemplate/src/main/java/co/edu/common/DbCommand.java
+++
@@ -1,8 +0,0 @@
-package co.edu.common;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface DbCommand {
-	public String execute(HttpServletRequest request, HttpServletResponse response);
-}
